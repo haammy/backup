772167b4df6ef44a82b44d1ea89643eb38458701
haammy <91935261+haammy@users.noreply.github.com>
2021-10-26 14:42:12 +0900

    Add files via upload

---
+++ doc/slick02.js
@@ -0,0 +1,34 @@
+$(function(){
+//----------------------------------------
+//메인슬라이드
+$('.main_slider').slick({
+    arrows:false,
+    autoplay:true,
+    pauseOnHover:false,
+    //<<위로 움직이는 슬라이드  vertical:true,
+    //<<흐려졌다 나오기 fade:true,
+
+});
+
+
+//제품슬라이드
+$(".pr_slider").slick({
+    arrows:false,
+    autoplay:true,
+    slidesToShow: 5,
+    dots:true,
+});
+
+$('#content01 .btn i.xi-arrow-left').on('click', function(){
+    $('.pr_slider').slick('slickPrev')
+});
+$('#content01 .btn i.xi-arrow-right').on('click', function(){
+    $('.pr_slider').slick('slickNext')
+});
+
+
+
+
+//--------------------------------------------
+
+})
