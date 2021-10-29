e4f88f0069319260aed7e173594b9d9580c54e4b
haammy <91935261+haammy@users.noreply.github.com>
2021-10-29 17:55:08 +0900

    Add files via upload

--- test01/js/onoma.js
+++ test01/js/onoma.js
@@ -9,6 +9,15 @@ $(function(){
     
     });
     
+    $('.pr_slider').slick({
+        arrows:false,
+        autoplay:true,
+        pauseOnHover:false,
+        //<<위로 움직이는 슬라이드  vertical:true,
+        //<<흐려졌다 나오기 fade:true,
+    
+    });
+    
     
     
     
