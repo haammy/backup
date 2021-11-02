897a52cd4c395f19c51c03c2cd6a38cdcb99e726
haammy <91935261+haammy@users.noreply.github.com>
2021-11-02 17:54:35 +0900

    Add files via upload

--- test01/js/onoma.js
+++ test01/js/onoma.js
@@ -9,13 +9,29 @@ $(function(){
     
     });
     
-    $('.pr_slider').slick({
+    $('.it_slider').slick({
         arrows:false,
         autoplay:true,
         pauseOnHover:false,
         //<<위로 움직이는 슬라이드  vertical:true,
         //<<흐려졌다 나오기 fade:true,
     
+    });
+
+
+    //제품 슬라이드
+    $(".pr_slider").slick({
+        arrows:false,
+        autoplay:true,
+        slidesToShow: 4,
+        
+    });
+    
+    $('#content04 .btn i.xi-angle-left-thin').on('click', function(){
+        $('.pr_slider').slick('slickPrev')
+    });
+    $('#content04 .btn i.xi-angle-right-thin').on('click', function(){
+        $('.pr_slider').slick('slickNext')
     });
     
     
