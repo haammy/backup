6ad063492cf53918cf0dce1ef3906df5f62cc9ca
haammy <91935261+haammy@users.noreply.github.com>
2021-11-03 14:07:31 +0900

    Add files via upload

---
+++ doc/basic05.js
@@ -0,0 +1,21 @@
+$(function(){
+//------------------------------------------------------
+$('#top_banner i').on('click', function(){
+    $('#top_banner ').hide(); 
+    //$('#top_banner ').slideUp(); >천천히 올라감
+});
+//function topBannerClose(){
+//    $('#top_banner i').hide();
+//}
+
+$('.main_slider').slick({
+    arrows:false,//버튼없애기
+    autoplay:true,//자동으로돌아라
+    autoplaySpeed:2000,//슬라이드속도 1000=1초
+
+})
+
+
+
+//------------------------------------------------------
+});
