304ce111c84fd356fee63e59deb4f9247483fd21
haammy <91935261+haammy@users.noreply.github.com>
2021-11-30 17:53:00 +0900

    Add files via upload

---
+++ pf04/js/hanjin.js
@@ -0,0 +1,15 @@
+$(function(){
+//-----------------------------------------
+$('.main_slider').slick({
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:2500,
+    pauseOnHover:false,
+    pauseOnFocus:false,
+    // dots:true,
+});
+
+
+
+//-----------------------------------------
+})
