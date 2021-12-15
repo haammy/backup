15399177e112f53271fab3e19f654b2aea34a3bf
haammy <91935261+haammy@users.noreply.github.com>
2021-12-16 02:10:02 +0900

    Add files via upload

--- pf05/js/benefit.js
+++ pf05/js/benefit.js
@@ -5,12 +5,17 @@ $('.top_banner i').on('click', function(){
     
 });
 
+$('.popup_close').on('click', function(){
+    $(this).parent().hide();
+});
+
+
 
 $("#utc").YTPlayer({
     videoURL:'https://youtu.be/QMNB-oG2yOk',
     containment:'#main_visual',
     autoPlay: true,
-    // mute:true,
+    mute:true,
     useOnMobile: true,
     loop: true,//반복
     startAt:0, 
@@ -19,8 +24,22 @@ $("#utc").YTPlayer({
 
 }); 
 
+$('.best_tap>li').on('click', function(){
+    var $this= $(this); //자기자신
+    var idx= $(this).index(); //자기자신의 번호 0 1 2
+    //console.log($this,idx);
+    //$('.tab_content>div').eq(idx).Show().siblings().hide();
+    $('.best_tap_content>div').eq(idx).addClass('on').siblings().removeClass('on');
+    $this.addClass('on').siblings().removeClass('on');
+});
 
+$('.content01 .click01 li').on('click', function(){
+    var idx= $(this).index(); //자기자신의 번호 0 1 2
+    $(".tab_content>div").eq(idx).addClass('on').siblings().removeClass('on');
+    $(this).addClass('on').siblings().removeClass('on');
+});
 
+AOS.init();
 
 
 
