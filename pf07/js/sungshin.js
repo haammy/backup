e040492991f01eb07711e27ea6af811eac22bcc6
haammy <91935261+haammy@users.noreply.github.com>
2021-12-30 18:59:29 +0900

    Add files via upload

--- pf07/js/sungshin.js
+++ pf07/js/sungshin.js
@@ -1,30 +1,43 @@
 $(function(){
-//------------------------------------------------------------
+    //------------------------------------------------------------
+    
+    $('#main').fullpage({
+        anchors:['c01', 'c02', 'c03', 'c04', 'footer'],
+        //navigation: true,
+        afterLoad: function(origin, destination, direction){
+            let txt = $('.navbar>li').eq(destination.index).find('a').text();
+            $('.section').eq(destination.index).addClass("on").siblings().removeClass('on');
+            $('.navbar>li').eq(destination.index).addClass("on").siblings().removeClass('on');
+            $('.now').text(txt);
+            
+    
+            destination.index==0 ? $('.top_pop').slideDown() : $('.top_pop').slideUp();
+            //destination.index==0 ? $('header').fadeIn() : $('header').fadeOut();
+            // destination.index==0 ? $('.right_pop').fadeIn() : $('.right_pop').fadeOut();
+        },
+    });
 
-$('#main').fullpage({
-    anchors:['c01', 'c02', 'c03', 'c04', 'footer'],
-    //navigation: true,
-    afterLoad: function(origin, destination, direction){
-        let txt = $('.navbar>li').eq(destination.index).find('a').text();
-        $('.section').eq(destination.index).addClass("on").siblings().removeClass('on');
-        $('.navbar>li').eq(destination.index).addClass("on").siblings().removeClass('on');
-        $('.now').text(txt);
+    
+    
+    //right_pop
+    $('.pop01>li>a').on('click', function(){        
+        $(this).next().slideToggle();
+        $(this).parent().siblings().find('.spop01').slideUp();
+    });
 
 
-        destination.index==0 ? $('.top_pop').fadeIn() : $('.top_pop').fadeOut();
-        // destination.index==0 ? $('.header').fadeIn() : $('.header').fadeOut();
-        destination.index==0 ? $('.right_pop').fadeIn() : $('.right_pop').fadeOut();
-    },
-});
+    //search
+    $('.right').on('click', function(){
+        $(this).toggleClass('on');
+        $('.all_menu').slideToggle();
+        if($('.right').hasClass('on')) {
+            $.fn.fullpage.setAllowScrolling(false);
+        }else{
+            $.fn.fullpage.setAllowScrolling(true);
+        }
+    });
 
-
-//right_pop
-$('.pop01>li>a').on('click', function(){        
-    $(this).next().slideToggle();
-    $(this).parent().siblings().find('.spop01').slideUp();
-});
-
-
-
-//------------------------------------------------------------
-})
+    
+    
+    //------------------------------------------------------------
+    })
