2470848b722dd01e142f88551138131c7705ba62
haammy <91935261+haammy@users.noreply.github.com>
2022-01-26 16:42:31 +0900

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,6 +5,7 @@ $(function(){
     let sideBar=$('nav li');
     $('#main').fullpage({
         anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'portfolio_06', 'profile'],
+        responsiveWidth:768,
         afterLoad: function(origin, destination, direction){
             
             let idx=destination.index;
